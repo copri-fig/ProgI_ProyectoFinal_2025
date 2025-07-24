48d2be08a8fcb3c3b9f12dd21ae8e391f6b92a97
Juan <jcopri206@gmail.com>
2025-07-24 14:27:45 -0500

    Creación de la clase Avion (vacia) y la clase Asiento que controla la reserva del asiento e imprime el boleto

--- src/ReservasAerolineaForm.java
+++ src/ReservasAerolineaForm.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
 
-public class ReservasAerolineaForm {
+public class ReservasAerolineaForm extends JFrame {
     private JPanel panelPrincipal;
     private JPanel panelDatos;
     private JTextField txtNombre;
@@ -19,4 +21,28 @@ public class ReservasAerolineaForm {
     private JButton btnLimpiar;
     private JButton btnMostrarBoleto;
     private JTextArea txtAreaInfo;
+
+    //Variables adicionales
+    private JButton[][] botonesEjecutivo;
+    private JButton[][] botonesEconomico;
+    private Avion avion;
+    private ButtonGroup grupoClase;
+    private ButtonGroup grupoPreferencia;
+
+    //Constructor
+    public ReservasAerolineaForm() {
+        avion = new Avion();
+
+
+    }
+    //Main
+    public static void main(String[] args) {
+        try{
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        new ReservasAerolineaForm().setVisible(true);
+    }
 }
