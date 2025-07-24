754e5d98ca18525eb4ea4c200a7af88753ee1228
Juan <jcopri206@gmail.com>
2025-07-24 15:41:30 -0500

    Creación del método setupButtonGroups para que los radiobuttons sean single choice.

--- src/ReservasAerolineaForm.java
+++ src/ReservasAerolineaForm.java
@@ -26,14 +26,51 @@ public class ReservasAerolineaForm extends JFrame {
     private JButton[][] botonesEjecutivo;
     private JButton[][] botonesEconomico;
     private Avion avion;
+    private Asiento asientoSeleccionado;
     private ButtonGroup grupoClase;
     private ButtonGroup grupoPreferencia;
 
     //Constructor
     public ReservasAerolineaForm() {
         avion = new Avion();
+        asientoSeleccionado = null;
 
+        //Configuración inicial del area de Información
+        txtAreaInfo.setText("=== SISTEMA DE RESERVAS AEROLÍNEA LÍNEA ===\n\n" +
+                "1. Ingrese los datos del pasajero\n" +
+                "2. Seleccione clase y preferencias\n" +
+                "3. Elija asignación automática o manual\n" +
+                "4. Presione 'Reservar' para confirmar\n\n" +
+                "LEYENDA DE COLORES:\n" +
+                "Verde: Disponible\n" +
+                "Rojo: Ocupado\n" +
+                "Amarillo: Seleccionado"
+        );
 
+        txtNombre.requestFocus();
+
+        //Configuración general
+        setContentPane(panelPrincipal);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
+        setLocationRelativeTo(null);
+
+        setupButtonGroups();
+
+    }
+
+    //Configura grupo de botones
+    private void setupButtonGroups() {
+        //grupo para las clases
+        grupoClase = new ButtonGroup();
+        grupoClase.add(rbEconomico);
+        grupoClase.add(rbEjecutivo);
+
+        //grupo para la preferencia
+        grupoPreferencia = new ButtonGroup();
+        grupoPreferencia.add(rbVentana);
+        grupoPreferencia.add(rbPasillo);
+        grupoPreferencia.add(rbCentro);
     }
     //Main
     public static void main(String[] args) {
