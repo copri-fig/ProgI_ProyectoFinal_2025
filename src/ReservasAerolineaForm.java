e7cd6c438e1159c2afff74339024e9b8fcd41151
Juan <jcopri206@gmail.com>
2025-07-23 19:18:40 -0500

    Creación del diseño del form para reservar el boleto

---
+++ src/ReservasAerolineaForm.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+public class ReservasAerolineaForm {
+    private JPanel panelPrincipal;
+    private JPanel panelDatos;
+    private JTextField txtNombre;
+    private JTextField txtCedula;
+    private JRadioButton rbEconomico;
+    private JRadioButton rbEjecutivo;
+    private JRadioButton rbVentana;
+    private JRadioButton rbPasillo;
+    private JCheckBox chkAutomatico;
+    private JRadioButton rbCentro;
+    private JPanel panelAsientos;
+    private JPanel panelEconomico;
+    private JPanel panelEjecutivo;
+    private JPanel panelInformacion;
+    private JButton btnReservar;
+    private JButton btnLimpiar;
+    private JButton btnMostrarBoleto;
+    private JTextArea txtAreaInfo;
+}
