aebcb67daa7fc4e9477987345a5b24dc8956ec20
Leonardo Morillo <leonardo.morillo@utp.ac.pa>
2025-07-26 12:50:03 -0500

    Se hizo el constructor de la clase, donde se puede observar que se desarrolla la disposición de los asientos, tanto de clase economica como ejecutiva.

--- src/Avion.java
+++ src/Avion.java
@@ -6,4 +6,24 @@ public class Avion {
 
     private Asiento[][] ejecutivo;
     private Asiento[][] economico;
+
+    public Avion() {
+        this.ejecutivo = new Asiento[FILAS_EJECUTIVO][ASIENTOS_POR_FILA_EJECUTIVO];
+        for (int i = 0; i < FILAS_EJECUTIVO; i++) {
+            ejecutivo[i][0] = new Asiento(i + 1, 1, "ventana");
+            ejecutivo[i][1] = new Asiento(i + 1, 2, "pasillo");
+            ejecutivo[i][2] = new Asiento(i + 1, 3, "pasillo");
+            ejecutivo[i][3] = new Asiento(i + 1, 4, "ventana");
+        }
+        this.economico = new Asiento[FILAS_ECONOMICO][ASIENTOS_POR_FILA_ECONOMICO];
+        for (int i = 0; i < FILAS_ECONOMICO; i++) {
+            economico[i][0] = new Asiento(i + 1, 1, "ventana");
+            economico[i][1] = new Asiento(i + 1, 2, "centro");
+            economico[i][2] = new Asiento(i + 1, 3, "pasillo");
+            economico[i][3] = new Asiento(i + 1, 4, "pasillo");
+            economico[i][4] = new Asiento(i + 1, 5, "centro");
+            economico[i][5] = new Asiento(i + 1, 6, "ventana");
+        }
+
+    }
 }
