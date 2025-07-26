db4b1676c77e91cc43b18fd30419c6ef587791e4
Leonardo Morillo <leonardo.morillo@utp.ac.pa>
2025-07-26 12:06:56 -0500

    Declaración Variables clase avión
    Se declararon las variables a en la clase avión, estas se utilizaran para la creación y organización de los asientos.

--- src/Avion.java
+++ src/Avion.java
@@ -1,2 +1,9 @@
 public class Avion {
+    private static final int FILAS_EJECUTIVO = 3;
+    private static final int ASIENTOS_POR_FILA_EJECUTIVO = 4;
+    private static final int FILAS_ECONOMICO = 6;
+    private static final int ASIENTOS_POR_FILA_ECONOMICO = 6;
+
+    private Asiento[][] ejecutivo;
+    private Asiento[][] economico;
 }
